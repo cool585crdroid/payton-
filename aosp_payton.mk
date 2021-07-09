0465987f038dce84a0c9af5ba5721a297efaa65a
cool585 <dhineshcool585@gmail.com>
2021-07-09 06:39:18 +0530

    payton: Update to coral July'21 fp
    Signed-off-by: cool585 <dhineshcool585@gmail.com>

--- aosp_payton.mk
+++ aosp_payton.mk
@@ -78,4 +78,4 @@ PRODUCT_RELEASE_NAME := payton
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=payton
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210705.001/7380771:user/release-keys"
